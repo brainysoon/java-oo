640aafa4c578c5f77a726487548131417e811e95
brainysoon <brainysoon@gmail.com>
2018-04-08 13:04:23 +0800

    [ Practice07 ] Implement person , student and teacher, student and teacher extend person, and teacher has introduceWith.

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,22 @@
 package practice07;
 
 public class Klass {
+
+    private Integer number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", this.number);
+    }
+
+    public boolean isSameClassOf(Klass klass) {
+        return this.number.equals(klass.getNumber());
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,38 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I teach %s.",
+                this.basicIntroduce(), this.klass == null ? "No Class" : this.klass.getDisplayName());
+    }
+
+    public String introduceWith(Student student) {
+        return String.format("%s I%s teach Jerry.", this.basicIntroduce(),
+                this.klass.isSameClassOf(student.getKlass()) ? "" : " don't");
+    }
+
+    private String basicIntroduce() {
+        return String.format("%s I am a Teacher.", super.introduce());
+    }
 }
