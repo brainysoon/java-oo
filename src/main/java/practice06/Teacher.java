204887044ee9d3a9f3730e66e20f71627201f9a3
brainysoon <brainysoon@gmail.com>
2018-04-08 12:51:41 +0800

    [ Practice06 ] Implement person , student and teacher, student and teacher extend person, and teacher has special case.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Teacher. I teach %s.",
+                super.introduce(), this.klass == null ? "No Class" : "Class " + this.klass);
+    }
 }
