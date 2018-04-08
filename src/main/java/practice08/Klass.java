ae3387e6a6b5bb4867a32e92056f6a06c93c143d
brainysoon <brainysoon@gmail.com>
2018-04-08 13:44:20 +0800

    [ Practice08 ] Implement person , student and teacher, student and teacher extend person, and person has id, klass has leader.

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,31 @@
 package practice08;
 
 public class Klass {
+
+    private Integer number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", this.number);
+    }
+
+    public boolean isSameClassOf(Klass klass) {
+        return this.number.equals(klass.getNumber());
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
