894292a6f8c3b491f012056f10bfd41c21050e4d
brainysoon <brainysoon@gmail.com>
2018-04-08 19:59:11 +0800

    [ Practice09 ] Implement person , student and teacher and give kalss a appendMember method.

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,45 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+
+    private Integer number;
+    private Student leader;
+    private List<Student> member;
+
+    public Klass(int number) {
+        this.number = number;
+        this.member = new ArrayList<>();
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", this.number);
+    }
+
+    public boolean isSameClassOf(Klass klass) {
+        return this.number.equals(klass.getNumber());
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (!member.contains(student)) {
+            this.leader = new Student(-1, "It's not a student", -1, this);
+            System.out.println("It is not one of us.");
+        } else {
+            this.leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        this.member.add(student);
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,46 @@
 package practice09;
 
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old.", this.name, this.age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.id == ((Person) obj).id;
+    }
 }
